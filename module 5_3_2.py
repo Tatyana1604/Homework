13cd54805cca0774d01f62ac2787b7f7ed6bb068
Tatyana1604 <tprutovaya@yandex.ru>
2024-07-17 11:08:58 +0300

    Add files via upload

---
+++ module 5_3_2.py
@@ -0,0 +1,27 @@
+class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        house_name = args[0]
+        object = super().__new__(cls)
+        cls.houses_history.append(house_name)
+        return object
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def __del__(self):
+        print(f"{self.args[0]},снесён, но он останется в истории ")
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
