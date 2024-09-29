e171d81148aa36b6aa152a150746f2c183d3716d
Tatyana1604 <tprutovaya@yandex.ru>
2024-09-29 12:11:45 +0300

    Update module 11_3.py

--- module 11_3.py
+++ module 11_3.py
@@ -1,22 +1,54 @@
-import inspect
-from pprint import pprint
+class Man:
+    def __init__(self, name=None):
+        if name:
+            self.name = name
+        else:
+            self.name = 'Макс'
+
+    def hello(self):
+        print('Я здесь', self.name)
+
+    def go(self, x=5, y=20):
+        print('перемещение', x, y)
 
 
-class Object:
-    pass
+man = Man()
 
 
-def introspection_info(obj):
-    type_ = type(obj).__name__
-    attribute = getattr(obj, '__dict__', None)
-    methods = dir(obj)
-    module = obj.__class__.__module__
-    func = inspect.isfunction(obj)
-    info = {'type': type_, 'attributes': attribute, 'methods': methods, 'module': module, 'function': func}
-    return info
+def some_function(param, param_2='n/a'):
+    print('my params is', param, param_2)
 
 
-obj = Object()
-number_info = introspection_info(42)
-pprint(number_info)
+
+
+def introspection(obj):
+    objDict = {}
+    objDict.setdefault('type', type(obj))
+    attributes = dir(obj)
+    obj_atr = []
+    obj_met = []
+    for method in attributes:
+        if callable(getattr(obj, method)):
+            obj_met.append(method)
+        else:
+            obj_atr.append(method)
+    objDict.setdefault('attributes', obj_atr)
+    objDict.setdefault('methods', obj_met)
+    try:
+        if obj.__module__:
+            objDict.setdefault('module', obj.__module__)
+        if obj.__name__:
+            objDict.setdefault('name', obj.__name__)
+    except AttributeError:
+        return objDict
+    return objDict
+
+
+number_info = introspection(42)
+print(number_info)
+
+number_info = introspection('red')
+print(number_info)
+number_info = introspection(man)
+print(number_info)
 
