2a2ba7939cdb77369158306465903ba4fff2195a
Tatyana1604 <tprutovaya@yandex.ru>
2024-10-20 20:32:59 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = 'KEY'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(text=['Urban', 'ff'])
+async def urban_message(message):
+    await message.answer('urban message')
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью. Рад вас видеть!')
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Не понимаю(( Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
