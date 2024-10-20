2348d8ca0f06e672876202c667ff7ea506fca651
Tatyana1604 <tprutovaya@yandex.ru>
2024-10-20 20:39:02 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,62 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+
+api = " "
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Для начала расчета введите слово Calories.')
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text='Calories')
+async def set_age(message):
+    await message.answer("Введите свой возраст")
+    await UserState.age.set()
+
+
+@dp.message_handler(state= UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer("Введите свой рост")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state= UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer('Введите свой вес')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    for_man = (10 * float(data['weight']) + 6.25 * float(data['growth']) -
+               5* float(data['age']) - 5)
+    for_woman = for_man - 166
+    await message.answer(f'Норма калорий для мужчин: {for_man}, для женщин: {for_woman}')
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
