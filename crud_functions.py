7a79cc6d88c270b61dfa9b8a4f4c209a892b6e8c
Tatyana1604 <tprutovaya@yandex.ru>
2024-11-15 09:47:18 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,28 +1,72 @@
 import sqlite3
 
-connection = sqlite3.connect("database.db")
-cursor = connection.cursor()
-
-
 def initiate_db():
+    connection = sqlite3.connect("ProdSan.db")
+    cursor = connection.cursor()
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT,
-    price INTEGER NOT NULL
-    );
+    price INTEGER
+    )
     ''')
+    connection.commit()
 
-    # for i in range(4):
-    #     cursor.execute("INSERT INTO Products(title, description, price) VALUES (?, ?, ?)",
-    #                    (f"Product{i+1}", f"Описание{i+1}", f"{(i+1)*100}"))
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')
+    connection.commit()
+    connection.close()
 
 
 def get_all_products():
-    products = cursor.execute("SELECT title, description, price FROM Products")
-    a = []
-    for i in products:
-        a += {f'Название: {i[0]} | Описание: {i[1]} | Цена: {i[2]}'}
+    connection = sqlite3.connect("ProdSan.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT title, description, price FROM Products")
+    users = cursor.fetchall()
+
+
     connection.commit()
-    return a
+    connection.close()
+    return users
+
+
+def add_user(username, email, age):
+    connection = sqlite3.connect("ProdSan.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM Users")
+    total_us = cursor.fetchone()[0]+1
+    cursor.execute(f'''
+    INSERT INTO Users VALUES('{total_us}', '{username}', '{email}', '{age}', '1000')
+    ''')
+
+    connection.commit()
+    connection.close()
+
+
+def is_included(username):
+    connection = sqlite3.connect("ProdSan.db")
+    cursor = connection.cursor()
+
+    is_inc = True
+    check_usnam = cursor.execute("SELECT * FROM Users WHERE username = ?", (username,))
+    if check_usnam.fetchone() is None:
+        is_inc = False
+
+    return is_inc
+
+
+    connection.commit()
+    connection.close()
+
+initiate_db()
