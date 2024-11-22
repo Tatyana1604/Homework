c37676173d0a8caa1286b8d8896937ee599efbaf
Tatyana1604 <tprutovaya@yandex.ru>
2024-11-22 22:09:07 +0300

    Add files via upload

--- module 16_1.py
+++ module 16_1.py
@@ -3,17 +3,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def welcome() -> dict:
+async def main_page() -> dict:
     return {"message": "Главная страница"}
 
 @app.get("/user/admin")
-async def welcome() -> dict:
+async def admin_page() -> dict:
     return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def usid(user_id:str) -> dict:
+async def user_page(user_id: int):
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
-@app.get("/user/{user_name}/{age}")
-async def usinf(user_name:str, age: str) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"}
+@app.get("/user")
+async def user_info(username: str = None, age: int = None):
+    if username and age:
+        return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+    return {"message": "Недостаточно данных. Укажите имя и возраст пользователя."}
